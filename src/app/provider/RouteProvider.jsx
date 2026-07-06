ed00e9ef40a2b2e44b020dadb89346663a649ae8
Zain Shaikh <szain2024@gmail.com>
2026-07-06 23:08:57 +0530

    Setup React Query

--- src/app/provider/RouteProvider.jsx
+++ src/app/provider/RouteProvider.jsx
@@ -1,8 +1,27 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "@/app/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10*60*1000,
+      retry:2,
+      refetchOnWindowFocus: false,
+    },
+    mutations:{
+      retry:0,
+    }
+  }
+})
 
 const AppRouterProvider = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />{" "}
+    </QueryClientProvider>
+  );
 };
 
 export default AppRouterProvider;
